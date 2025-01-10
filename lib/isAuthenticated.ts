3a1e5df9badfeb730e6c53c7fb1627d0f8a3f63d
Jahid0178 <zahidul.haque767@gmail.com>
2025-01-11 03:57:03 +0600

    feat: add `isAuthenticated` utility function for user authentication check
    - Created `isAuthenticated` function to verify user authentication status.
    - Fetches user data from the backend `/api/users/me` endpoint using Axios.
    - Utilizes `cookies` from `next/headers` to include authentication cookies in the request.
    - Handles authentication response, returning user data and login status.
    - Provides error handling to log issues and return a fallback response.

---
+++ lib/isAuthenticated.ts
@@ -0,0 +1,31 @@
+import axios from "axios";
+import { cookies } from "next/headers";
+
+async function isAuthenticated() {
+  try {
+    const cookieStore = await cookies();
+    const cookie = cookieStore.toString();
+
+    const response = await axios.get(
+      `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/users/me`,
+      {
+        headers: {
+          Cookie: cookie,
+        },
+      }
+    );
+
+    return {
+      user: response.data,
+      loggedIn: true,
+    };
+  } catch (error) {
+    console.log("authenticated error", error);
+    return {
+      user: null,
+      loggedIn: false,
+    };
+  }
+}
+
+export default isAuthenticated;
